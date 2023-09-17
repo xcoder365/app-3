aa29e04aa1f3686a0f978dff342cb5c916c85137
xcoder365 <144996132+xcoder365@users.noreply.github.com>
2023-09-17 15:13:57 -0700

    app-3

--- app3.py
+++ app3.py
@@ -1,33 +1,10 @@
+
 import streamlit as st
-import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.DataFrame(
-    {
-        "Animal": ["Lion", "Elephant", "Giraffe", "Monkey", "Zebra"],
-        "Habitat": ["Savanna", "Forest", "Savanna", "Forest", "Savanna"],
-        "Lifespan (years)": [15, 60, 25, 20, 25],
-        "Average weight (kg)": [190, 5000, 800, 10, 350],
-    }
-)
+medals =  pd.read_csv('data.csv')
 
-def dataframe_with_selections(df):
-    df_with_selections = df.copy()
-    df_with_selections.insert(0, "Select", False)
-
-    # Get dataframe row-selections from user with st.data_editor
-    edited_df = st.data_editor(
-        df_with_selections,
-        hide_index=True,
-        column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-        disabled=df.columns,
-    )
-
-    # Filter the dataframe using the temporary column, then drop the column
-    selected_rows = edited_df[edited_df.Select]
-    return selected_rows.drop('Select', axis=1)
-
-
-selection = dataframe_with_selections(df)
-st.write("Your selection:")
-st.write(selection)
+fig, ax = plt.subplots()
+medals.plot.bar(x='Country', y = ['Gold','Silver','Bronze'],ax=ax)
+st.pyplot(fig)
